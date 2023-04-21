874422c946a2f50232e79352ffd04d2486ae956a
Nikhil KUmar <131350052+nikhilkr67@users.noreply.github.com>
2023-04-21 22:05:05 +0530

    Add files via upload

--- repositories/EmployeeRepository.java
+++ repositories/EmployeeRepository.java
@@ -1,1 +1,10 @@
+package com.ems.repositories;
 
+import com.ems.entities.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+
+}
