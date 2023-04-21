129f6e89297195d52fab04257265a62431525cd1
Nikhil KUmar <131350052+nikhilkr67@users.noreply.github.com>
2023-04-21 22:05:28 +0530

    Add files via upload

---
+++ repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.ems.repositories;
+
+import com.ems.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long> {
+    User findByEmail(String email);
+}
