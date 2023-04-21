9608ccaf48623b6311ada91fb49708fc788d886d
Nikhil KUmar <131350052+nikhilkr67@users.noreply.github.com>
2023-04-21 22:02:49 +0530

    Add UserRegiservationDto.java file

--- DTO/UserRegistrationDto.java
+++ DTO/UserRegistrationDto.java
@@ -1,1 +1,12 @@
+package com.ems.payload;
 
+import lombok.Data;
+
+@Data
+public class UserRegistrationDto {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+}
